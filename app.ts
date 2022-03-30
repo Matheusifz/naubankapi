f8f36ac2f7c4444f0ea99917de4623ebb4d12da6
matheusifz <matheusifz@gmail.com>
2022-03-30 16:23:33 -0300

    feat: login

--- app.ts
+++ app.ts
@@ -12,12 +12,12 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    if (!(email || password || username  )) {
+    if (!(email || password || username)) {
       return res.status(400).send("All input are required");
     }
     const oldUser = await User.findOne({ email });
     if (oldUser) {
-      return res.send("User Already Exist. Please Login");
+      return res.send("User Already Exist. Please Log in");
     }
 
     const encryptedUserPassword = await bcrypt.hash(password, 10);
@@ -41,7 +41,29 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    res.status(400).send("Incorrect Password/Email");
+  }
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY ?? "",
+      {
+        expiresIn: "5h",
+      }
+    );
+
+    user.token = token;
+
+    return res.status(200).json(user);
+  }
+
+  return res.status(400).send("Invalid Credentials");
 });
 
 export default app;
